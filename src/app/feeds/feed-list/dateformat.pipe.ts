c8b3172b1eb5747ceabe9fbbced610ae0f75b991
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2019-03-31 15:21:27 -0300

    fix date pipe

--- src/app/feeds/feed-list/dateformat.pipe.ts
+++ src/app/feeds/feed-list/dateformat.pipe.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform } from "@angular/core";
+import * as moment from 'moment';
 
 @Pipe({
   name: "dateFormat"
@@ -9,15 +10,16 @@ export class DateFormatPipe implements PipeTransform {
     const date: Date = new Date(value);
     var timeDiff = date.getTime() - now.getTime();
     var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    if (date.getDate() === now.getDate() - 1) {
+      return "Yesterday";
+    }
     switch (diffDays) {
-      case -1:
-        return "Yesterday";
       case 0:
         const formattedMinutes = (date.getUTCMinutes().toString().length === 1)? "0"+date.getUTCMinutes().toString() : date.getUTCMinutes().toString();
         const momentOfDay = (date.getUTCHours() < 12)? "am" : "pm";
         return date.getUTCHours().toString() + ":" + formattedMinutes + " " + momentOfDay;
       default:
-        return date.getMonth() + " " + date.getDay();
+        return moment(value).format('MMM D');
     }
   }
 }
